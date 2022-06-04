784289a65dcc52b5f4d930a30a05bc41b79097b1
Willians Duabyakosky <will_dex@hotmail.com>
2022-06-03 21:53:03 -0400

    Agregar archivos de proyecto.

---
+++ Horizonte Ganadero/Horizonte Ganadero/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Horizonte_Ganadero
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BrowserLink.Source = "https://horizonteganadero.tv/";
+        }
+    }
+}
